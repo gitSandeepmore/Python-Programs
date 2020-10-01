48ce4805903cfddbcbb9523094b25b29fc4293d1
gitSandeepmore <72209633+gitSandeepmore@users.noreply.github.com>
2020-10-01 23:23:18 +0530

    Added Simple_Interest program

---
+++ Simple_Interest.py
@@ -0,0 +1,11 @@
+# https://www.facebook.com/sandeepmore.more.5/posts/1261804104181092
+bscribed by code House
+
+
+
+p=float(input("Enter principle:"))
+r=float(input("Enter rate of interest:"))
+t=float(input("Enter time duration:"))
+# The formula for the simple interest is:-
+SI=(p*r*t)/100
+print("Simple Interest:",SI)
